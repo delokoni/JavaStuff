2a12beec8132d58b2829b908ae2b8c0f2aca8e0a
kheidt <konstantin.heidt@gmail.com>
2017-10-24 00:43:14 +0200

    some new stuff

---
+++ src/Emily.java
@@ -0,0 +1,8 @@
+public class Emily {
+    public static void main(String []args){
+        System.out.println("A bird came down the walk");
+        System.out.println("He did not know I saw");
+        System.out.println("He bid an angel worm in halves");
+        System.out.println("And ate the fellow - raw");
+    }
+}
